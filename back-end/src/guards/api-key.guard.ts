5b5c918d58341c7efa3be2a395cf7300745cceb7
jdvpl <juanda554242@gmail.com>
2025-02-09 13:36:57 -0500

    feat: add ApiKeyGuard for API key authentication

---
+++ back-end/src/guards/api-key.guard.ts
@@ -0,0 +1,18 @@
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  UnauthorizedException,
+} from '@nestjs/common';
+
+@Injectable()
+export class ApiKeyGuard implements CanActivate {
+  canActivate(context: ExecutionContext): boolean {
+    const request = context.switchToHttp().getRequest();
+    const apiKey = request.headers['x-api-key'];
+    if (apiKey !== process.env.API_KEY) {
+      throw new UnauthorizedException('Invalid API Key');
+    }
+    return true;
+  }
+}
